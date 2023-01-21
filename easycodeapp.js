b8b9d268606ee117ab3f06cca9d23642b38799d8
collectors crypt <123226609+collectorscrypto@users.noreply.github.com>
2023-01-20 23:27:22 -0800

    Add files via upload

---
+++ easycodeapp.js
@@ -0,0 +1,92 @@
+class lang {
+    constructor(num1, num2, num3, num4, num5, num6, num7){
+       this.num1 = num1;
+       this.num2 = num2;
+       this.num3 = num3;
+       this.num4 = num4;
+       this.num5 = num5;
+       this.num6 = num6;
+       this.num7 = num7;
+    }
+
+}
+
+var htmllangtags_ = new lang ('<head></head>', '<body></body>', '<script></script>','<title></title>','<div><div>','<p><p>','<h><h>');
+var jslang_ = new lang ('var xwhatx = new xwhatx(xwhatx)', 'console.log(xwhatx)', 'class lang { constructor(num1, num2, num3, num4, num5) } this.num1 = num1','let = [xwhatx, xwhatx] xwhatx(xwhatx)','if (xwhatx >= xwhatx && xwhat < xwhatx) xwhatx(..)','<link rel="stylesheet" href="./YOUR FILE NAME.css">');
+var jshtmlang_ = new lang ('for(var i=0; i< 10; i++){ (i) } ', '<script src="./"></script>', 'document.write(..)', '<button onclick="greet()"></button>', 'function greet(xwhatx) {xwhatx}')
+
+
+
+
+
+console.log(htmllangtags_)
+console.log(jslang_)
+console.log(jshtmlang_)
+
+
+function greet() {
+    console.log ('htmltags ' + htmllangtags_.num1);;
+}
+function great() {
+   document.write (console);
+}
+function reet() {
+    console.log ('htmltags ' + htmllangtags_.num2);
+}
+function qeet() {
+    console.log ('htmltags ' + htmllangtags_.num6);
+}
+function aeet() {
+    console.log ('htmltags ' + htmllangtags_.num7);
+}
+function seat() {
+    console.log('htmltags ' + htmllangtags_.num3);
+}
+function zeet() {
+    console.log('htmltags ' + htmllangtags_.num4);
+}
+function yeet() {
+    console.log('htmltags ' + htmllangtags_.num5); 
+}
+function teet() {
+    console.log('htmltags ' + jslang_.num1);
+}
+function meet() { 
+    console.log('htmltags ' + jslang_.num2);
+}
+function geet() {
+    console.log('htmltags ' + jslang_.num3);
+}
+function leet() { 
+    console.log('htmltags ' + jslang_.num4);
+}
+function xeet() { 
+    console.log('htmltags ' + jslang_.num5);   
+}
+function veet() { 
+    console.log('htmltags ' + jslang_.num6);   
+}
+function feet() {
+    console.log('htmltags ' + jshtmlang_.num1);
+}
+function beet() {
+    console.log('htmltags ' + jshtmlang_.num2);
+}
+function weet() {
+    console.log('htmltags ' + jshtmlang_.num3);
+}
+function ceet() {
+    console.log('htmltags ' + jshtmlang_.num4);
+}
+function peet() {
+    console.log(jshtmlang_.num5);
+}    
+
+function jam(){
+
+console.log('time');
+}
+
+
+
+
